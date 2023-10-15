55eb6b88909b006eee98c85c0488751b3e783b40
ssx <793185273@qq.com>
2023-10-15 13:25:19 +0800

    change

--- model/RNNQnet.py
+++ model/RNNQnet.py
@@ -3,44 +3,23 @@ import pdb
 import torch.nn
 from torch import nn
 import collections
-class DRQNetwork(nn.Module):
+class RNNQnet(nn.Module):
     def __init__(self, n_observations, n_actions):
         super().__init__()
-        #self.with_bias = True
-        '''
-        self.fc1 = nn.Linear(n_observations, 128, bias=self.with_bias)
-        self.fc2 = nn.Linear(128, 64, bias=self.with_bias)
-        self.fc3 = nn.Linear(64, 32, bias=self.with_bias)
-        self.fc4 = nn.Linear(32, n_actions, bias=self.with_bias)
-        '''
-        self.obshape = n_observations
 
         self.hidden_size = 64
         self.net1 = nn.Sequential(collections.OrderedDict([
             ('fc1', nn.Linear(n_observations, 64)),
             ('relu1', nn.ReLU())
         ]))
-        self.lstm = torch.nn.LSTM(self.hidden_size, self.hidden_size, 1, batch_first=True)
-        self.net2 = nn.Sequential(collections.OrderedDict([
-            ('fc2', nn.Linear(64, 32)),
-            ('relu2', nn.ReLU()),
-            ('fc3', nn.Linear(32, n_actions))
-            ]))
+        self.rnn = nn.GRUCell(self.hidden_size, self.hidden_size)
+        self.net2 = nn.Linear(self.hidden_size, n_actions)
 
-    def init_lstm(self, batch_size=None, device=None, train=False):
-        if train:
-            h = torch.zeros([1, batch_size, self.hidden_size], dtype=torch.float).to(device)
-            c = torch.zeros([1, batch_size, self.hidden_size], dtype=torch.float).to(device)
-        else:
-            h = torch.zeros([1, 1, self.hidden_size], dtype=torch.float).to(device)
-            c = torch.zeros([1, 1, self.hidden_size], dtype=torch.float).to(device)
-        return h, c
-
-    def forward(self, state, h, c):
+    def forward(self, state, hidden_state):
         x = self.net1(state)
-        x, (n_h, n_c) = self.lstm(x, (h, c))
-        x = self.net2(x)
-        return x, n_h, n_c
+        h = self.rnn(x, hidden_state)
+        q = self.net2(h)
+        return q, h
 
 
 
