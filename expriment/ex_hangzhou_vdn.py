ce2f7e48f30543e92275d6f4fe15fcf2875f0a49
ssx <793185273@qq.com>
2023-10-17 22:33:53 +0800

    test_args

--- expriment/ex_hangzhou_vdn.py
+++ expriment/ex_hangzhou_vdn.py
@@ -24,11 +24,11 @@ def seed_torch(seed):
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
-if __name__ == '__main__':
+def run(test, lr, tf, bs):
     prs = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    prs.add_argument("-route", dest="route", type=str, default='../nets/hangzhou/hangzhou_4x4_gudang_18041610_1h.rou.xml')
-    prs.add_argument("-net", dest="net",default='../nets/hangzhou/hangzhou_4x4_gudang_18041610_1h.net.xml')
+    prs.add_argument("-route", dest="route", type=str,
+                     default='../nets/hangzhou/hangzhou_4x4_gudang_18041610_1h.rou.xml')
+    prs.add_argument("-net", dest="net", default='../nets/hangzhou/hangzhou_4x4_gudang_18041610_1h.net.xml')
     prs.add_argument("-lr", dest="lr", default=0.001)
     prs.add_argument("-gamma", dest="gamma", default=0.99)
     prs.add_argument("-gradient_step", dest="gradient_step", default=1)
@@ -42,20 +42,23 @@ if __name__ == '__main__':
     prs.add_argument("-delta_time", dest="delta_time", default=5)
     prs.add_argument("-buffer_size", dest="buffer_size", default=36000)
     prs.add_argument("-start_size", dest="start_size", default=300)
-    prs.add_argument("-reward", dest="reward", default="queue") #"queue", "pressure", "diffwait", "speed"
-    prs.add_argument("-alg", dest="alg", default="idqn") #"idqn", "vdn", "qmix"
+    prs.add_argument("-reward", dest="reward", default="queue")  # "queue", "pressure", "diffwait", "speed"
+    prs.add_argument("-alg", dest="alg", default="idqn")  # "idqn", "vdn", "qmix"
 
     args = prs.parse_args()
+    if test:
+        args.lr = lr
+        args.train_freq = tf
+        args.buffer_size = bs
     args.seed = 'random'
-    exprimenttime = str(datetime.now()).split('.')[0]
+    exprimenttime = str(datetime.now()).split('.')[0].replace(':', '-')
     csv_name = '../output/hangzhou/{}_single_st_{}_{}/{}_{}_{}_{}'.format(args.alg, args.reward, exprimenttime,
-                                                                      args.lr,
-                                                                      args.gradient_step,
-                                                                      args.train_freq,
-                                                                      args.target_update_freq)
+                                                                          args.lr,
+                                                                          args.gradient_step,
+                                                                          args.train_freq,
+                                                                          args.target_update_freq)
     param_file = '../output/hangzhou/{}_single_st_{}_{}/'.format(args.alg, args.reward, exprimenttime)
-    xml_file = "../output/hangzhou/xml/"
-    #args.xml_file = xml_file
+    xml_file = param_file + 'xml/'
     os.makedirs(os.path.dirname(csv_name), exist_ok=True)
     os.makedirs(os.path.dirname(xml_file), exist_ok=True)
     args.param_file = param_file
@@ -74,7 +77,7 @@ if __name__ == '__main__':
         random.seed(seed)
         seed_torch(seed)
 
-    #pdb.set_trace()
+    # pdb.set_trace()
     env = MALenv(args)
     init_st = env.reset()
     args.agent_ids = env.agent_id
@@ -88,7 +91,6 @@ if __name__ == '__main__':
     elif args.alg == "qmix":
         from agent.QMIXagent import Agent
     agents = Agent(args)
-
 
     max_episode = 200
     start_time = time.time()
@@ -104,12 +106,31 @@ if __name__ == '__main__':
             env.save_sim_info()
             env.save_episode_info(agents=agents)
         env.close()
-        if ep != max_episode-1:
+        if ep != max_episode - 1:
             new_st = env.reset()
             agents.reset_st(state=new_st)
-        print(f"ep{ep} cost {time.time()-ep_start}")
+        print(f"ep{ep} cost {time.time() - ep_start}")
     if args.save_param:
         agents.save_parameters()
     end_time = time.time()
-    print('cost time:', end_time-start_time, 's')
+    print('cost time:', end_time - start_time, 's')
+
+
+if __name__ == '__main__':
+    test_arg = True
+    lr_list = [0.0005, 0.001, 0.002, 0.004]
+    train_freq_list = [1, 2, 4, 8]
+    buffer_size_list = [3600, 7200, 14400, 36000]
+    test_lists = {
+        'lr': lr_list,
+        'train_freq': train_freq_list,
+        'buffer_size': buffer_size_list
+    }
+    if test_arg:
+        for test_lr in lr_list:
+            for test_tf in train_freq_list:
+                for test_bs in buffer_size_list:
+                    run(test_arg, test_lr, test_tf, test_bs)
+    else:
+        run(test_arg)
 
